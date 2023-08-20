37082bcb50ae0816f9ef4f636c9b6abe99fa2912
asif-shahriar11 <asif.asr11@gmail.com>
2023-08-21 03:28:20 +0600

    home-modified

--- app/page.js
+++ app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className={"flex justify-center items-center h-10vh] ml-10"} style={{ textAlign: 'center' }}>
+        <div className={"mb-16 flex justify-center items-center h-10vh] ml-10"} style={{ textAlign: 'center' }}>
           <div className="flex flex-col items-center">
             <img src="/assets/offline_visit.png" alt="Image 1" className="mb-2" style={{ height: '100px' }}/>
             <h3 className={"text-2xl font-bold"} style={{ color: 'black' }}>
@@ -86,68 +86,98 @@ export default function Home() {
           </div>
         </div>
 
-
-
-        {/* <div className={"flex justify-center items-center h-[50vh] ml-10"}>
-  <div className={"flex flex-col gap-5 flex-1 flex-row"}>
-    <div className="flex-1"> 
-      <h3 className={"text-2xl font-bold"} style={{ color: 'violet', textAlign: 'center' }}>
-        We at WhiskerDoc are here to help you and your pet <br />
-        enjoy a stressless and healthy life.
-      </h3>
-    </div>
-    <div className="flex flex-col items-center flex-1"> 
-      <h3 className={"text-2xl font-bold"} style={{ color: 'black' }}>
-        Something unusual with your pet?
-      </h3>
-      <div className="mt-2" style={{ color: 'black' }}>
-        Check out our articles written by professionals, <br />
-        to help you understand your pet's health better.
-      </div>
-    </div>
-    <div className="ml-4 flex flex-col items-center flex-1"> 
-      <h3 className={"text-2xl font-bold"} style={{ color: 'black' }}>
-        How to make your pet's life better?
-      </h3>
-      <div className="mt-2" style={{ color: 'black' }}>
-        Check out our blog <br />
-        from the comfort of your and your buddy's home.
-      </div>
-    </div>
-  </div>
-</div> */}
-
         
-        <div className={"flex justify-center items-center h-[25vh] ml-10"} >
+
+        <div className={"mb-20 flex justify-between items-center h-[60vh] ml-10"} style={{ backgroundImage: 'url("/assets/your_cat.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
           <div className={"flex flex-col gap-5 flex-1"}>
-            <div  style={{ textAlign: 'center' }}>
-              <h3 className={"text-2xl font-bold"} style={{ color: 'violet', textAlign: 'center' }}>
-                We at WhiskerDoc are here to help you and your pet <br />
-                enjoy a stressless and healthy life.
-              </h3>
+            <div style={{ color: 'red', paddingLeft: '40px'}}>
+              <h1 className={"text-2xl font-bold"}>
+                We at WhiskerDoc are committed to help you and your pet <br />
+                enjoy a stressless and healthy life.            
+              </h1>
             </div>
           </div>
         </div>
+
+
+
+        <div className={"mb-50 flex justify-center items-center h-[1vh] ml-10"} style={{ textAlign: 'center' }}>
+          <div style={{ marginTop: '50px' }}>
+            <div className="flex gap-4">
+              <div className="bg-white rounded-lg shadow-md p-4 flex-1">
+                <h3 className={"text-2xl font-bold text-black"}>
+                  Something unusual with your pet?
+                </h3>
+                <div className="mt-2 text-black">
+                  Check out our articles written by professionals, <br />
+                  to help you understand your pet's health better.
+                </div>
+              </div>
+              <div className="bg-white rounded-lg shadow-md p-4 flex-1">
+                <h3 className={"text-2xl font-bold text-black"}>
+                  How to make your pet's life better?
+                </h3>
+                <div className="mt-2 text-black">
+                  Check out our blog, you'll learn new things. <br />
+                  Additionally, you can help others by writing your own.
+                </div>
+              </div>
+              <div className="bg-white rounded-lg shadow-md p-4 flex-1">
+                <h3 className={"text-2xl font-bold text-black"}>
+                  Choose a subscription plan that fits your needs.
+                </h3>
+                <div className="mt-2 text-black">
+                  You can take 6-months or 12-months subscription plan, <br />
+                  or no subscription at all. Choose what suits you best.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+        <div className={"mb-20 flex justify-between items-center h-[60vh] ml-10"} style={{ backgroundImage: 'url("/assets/vet.jpg")', backgroundSize: 'cover', backgroundPosition: 'center',  marginTop: '150px' }}>
+          <div className={"flex flex-col gap-5 flex-1"}>
+            <div style={{ color: 'red', paddingLeft: '40px'}}>
+              <h1 className={"text-2xl font-bold"}>
+                Are you a veterinarian? <br />
+                WhiskerDoc is just the place.            
+              </h1>
+            </div>
+          </div>
+        </div>
+
+
+
         <div className={"flex justify-center items-center h-[1vh] ml-10"} style={{ textAlign: 'center' }}>
-          <div className="flex flex-col items-center">
-            <h3 className={"text-2xl font-bold"} style={{ color: 'black' }}>
-              Something unusual with your pet?
-            </h3>
-            <div className="mt-2" style={{ color: 'black' }}>
-              Check out our articles written by professionals, <br />
-              to help you understand your pet's health better.
-            </div>
-          </div>
-          <div className="ml-4 flex flex-col items-center">
-            <h3 className={"text-2xl font-bold"} style={{ color: 'black' }}>
-              How to make your pet's life better?
-            </h3>
-            <div className="mt-2" style={{ color: 'black' }}>
-              Check out our blog <br />
-              from the comfort of your and your buddy's home.
+          <div style={{ marginTop: '50px' }}> {/* Add a margin to this wrapper */}
+            <div className="flex gap-4">
+              <div className="bg-white rounded-lg shadow-md p-4 flex-1">
+                <h3 className={"text-2xl font-bold text-black"}>
+                  Automation to help you focus on what matters.
+                </h3>
+                <div className="mt-2 text-black">
+                  Use our automated prescription system, <br />
+                  to save time and effort and to focus on the fur-babies.
+                </div>
+              </div>
+              <div className="bg-white rounded-lg shadow-md p-4 flex-1">
+                <h3 className={"text-2xl font-bold text-black"}>
+                  Share your expertise with the world.
+                </h3>
+                <div className="mt-2 text-black">
+                  Write articles on various pet syndromes, <br />
+                  to help pet owners understand their pets better.
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
+
+
+
 
         
 
@@ -156,8 +186,3 @@ export default function Home() {
     </>
   );
 }
-
-
-// export default function Home() {
-//   redirect("/login");
-// }
